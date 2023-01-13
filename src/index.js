b62ba50d6fef04dfcbed8651de35f0320de96683
Barbsp <barbra.spg@gmail.com>
2023-01-12 23:23:04 -0500

    added main icon auto

--- src/index.js
+++ src/index.js
@@ -54,7 +54,7 @@ function convertToCelsius(event) {
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
-
+/*
 //City input to display on h1
 //Also where I make the API call to Open weather API
 function userSearchSubmit(event) {
@@ -100,5 +100,57 @@ let currentLocationBtn = document.querySelector("#current-loc-btn");
 currentLocationBtn.addEventListener("click", displayCurrentLocation);
 
 searchCity("New York");
-
+*/
 //5 days forecast
+
+function userSearchSubmit(event) {
+  event.preventDefault();
+  let apiKey = "7670b5bad10at2acef43458f5a3o2b37";
+  let city = document.querySelector("#city-input").value;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayCurrentWeather);
+  searchCity(city);
+}
+
+function displayCurrentWeather(response) {
+  let iconElement = document.querySelector("#weatherIcon");
+
+  console.log(response);
+  document.querySelector("#city").innerHTML = response.data.city;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.temperature.current
+  );
+  document.querySelector("#humid").innerHTML =
+    response.data.temperature.humidity;
+  document.querySelector("#description").innerHTML =
+    response.data.condition.description;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  iconElement.setAttribute(
+    "src",
+    `https://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+}
+
+//Default location display (for the start)
+function searchCity(city) {
+  let apiKey = "7670b5bad10at2acef43458f5a3o2b37";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayCurrentWeather);
+}
+function displayCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+function showPosition(position) {
+  let apiKey = "f0d090e44b50c091a69364839fa23474";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayCurrentWeather);
+}
+let currentLocationBtn = document.querySelector("#current-loc-btn");
+currentLocationBtn.addEventListener("click", displayCurrentLocation);
+
+searchCity("New York");
